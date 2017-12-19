71efb8874e6c5f70eb41660099947680e4cbb260
Bernard Godard <bgodard@users.noreply.github.com>
2017-12-19 22:23:07 +0100

    Added prefetch method and misc cleanup.

--- src/CALCEPH.jl
+++ src/CALCEPH.jl
@@ -9,25 +9,41 @@ end
 
 type CalcephEphem
    data :: Ptr{Void}
-   function CalcephEphem(files::Vector{T} where T<:AbstractString)
+   function CalcephEphem(files::Vector{<:AbstractString})
       ptr = ccall((:calceph_open_array, libcalceph), Ptr{Void}, (Int, Ptr{Ptr{UInt8}}), length(files), files)
       if (ptr == C_NULL)
          error("Unable to open ephemeris file(s)!")
-      else
-         res = new(ptr)
-         finalizer(res,_cleanup)
-         res
       end
+      obj = new(ptr)
+      finalizer(obj,CalcephEphemDestructor) # register object destructor
+      return obj
    end
 end
 
-function _cleanup(e::CalcephEphem)
+# to be called by gc when cleaning up
+# not in the exposed interface but can be called with finalize(e)
+function CalcephEphemDestructor(e::CalcephEphem)
+   if (e.data == C_NULL) 
+      return
+   end
    ccall((:calceph_close, libcalceph), Void, (Ptr{Void},), e.data) 
-   e.data = C_NULL    
+   e.data = C_NULL
+   return  
 end
 
 CalcephEphem(file::AbstractString) = CalcephEphem([file])
 
-export CalcephEphem
+function CalcephPrefetch(e::CalcephEphem) 
+    if (e.data == C_NULL)
+       error("Ephemeris object is not propely initialized.")
+    end
+    stat = ccall((:calceph_prefetch, libcalceph), Int, (Ptr{Void},), e.data)
+    if (stat == 0)
+       error("Unable to prefetch ephemeris!")
+    end
+    return
+end
+
+export CalcephEphem, CalcephPrefetch
 
 end # module
