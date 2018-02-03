a6654d0b7944f6b05df4f5dc0ce643c2ff140cd3
Bernard Godard <bgodard@users.noreply.github.com>
2018-02-03 19:24:33 +0100

    fix doc deployment

--- docs/make.jl
+++ docs/make.jl
@@ -4,6 +4,6 @@ makedocs(modules=[CALCEPH],
         doctest=true)
 
 deploydocs(deps   = Deps.pip("mkdocs", "python-markdown-math"),
-    repo = "github.com/bgodard/CALCEPH.git",
+    repo = "github.com/bgodard/CALCEPH.jl.git",
     julia  = "0.6",
     osname = "linux")
