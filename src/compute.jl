1b8a1119137091e7e652cb6b995b04c0655c35d1
Bernard Godard <bgodard@users.noreply.github.com>
2018-02-05 17:07:30 +0100

    fix typo

--- src/compute.jl
+++ src/compute.jl
@@ -1,11 +1,11 @@
 """
-    compute(eph,JD0,time,target,center)
+    compute(eph,jd0,time,target,center)
 
 Compute position and velocity of target with respect to center at epoch
-JD0+time. This method does not support the NAIF numbering scheme.
+jd0+time. This method does not support the NAIF numbering scheme.
 To get the best precision for the interpolation, the time is splitted in two
-floating-point numbers. The argument JD0 should be an integer and time should
-be a fraction of the day. But you may call this function with time=0 and JD0,
+floating-point numbers. The argument jd0 should be an integer and time should
+be a fraction of the day. But you may call this function with time=0 and jd0,
 the desired time, if you don't take care about precision.
 
 This method does not support the NAIF body identification scheme.
@@ -16,8 +16,8 @@ Output units are:
 * second and unitless for time ephemeris and time ephemeris rate
 
 # Arguments
-- `JD0::Float64`: JD0+time must be equal to the Julian date for the time coordinate corresponding to the ephemeris (usually TDB or TCB)
-- `time::Float64`: JD0+time must be equal to the Julian date for the time coordinate corresponding to the ephemeris (usually TDB or TCB)
+- `jd0::Float64`: jd0+time must be equal to the Julian Day for the time coordinate corresponding to the ephemeris (usually TDB or TCB)
+- `time::Float64`: jd0+time must be equal to the Julian Day for the time coordinate corresponding to the ephemeris (usually TDB or TCB)
 - `target::Int`: The body or reference point whose coordinates are required.
 - `center::Int`: The origin of the coordinate system.
 
@@ -41,59 +41,59 @@ The possible values for target and center are :
 * asteroid number + CALCEPH_ASTEROID    : asteroid
 
 """
-function compute(eph::Ephem,JD0::Float64,time::Float64,
+function compute(eph::Ephem,jd0::Float64,time::Float64,
    target::Int64,center::Int64)
     @_checkPointer eph.data "Ephemeris is not properly initialized!"
     result = Array{Float64,1}(6)
     stat = ccall((:calceph_compute, libcalceph), Cint,
     (Ptr{Void},Cdouble,Cdouble,Cint,Cint,Ref{Cdouble}),
-    eph.data,JD0,time,target,center,result)
+    eph.data,jd0,time,target,center,result)
     @_checkStatus stat "Unable to compute ephemeris"
     return result
 end
 
 """
-    compute(eph,JD0,time,target,center,unit)
+    compute(eph,jd0,time,target,center,unit)
 
 Compute position and velocity of target with respect to center
-at epoch JD0+time.
+at epoch jd0+time.
 To get the best precision for the interpolation, the time is splitted in two
-floating-point numbers. The argument JD0 should be an integer and time should
-be a fraction of the day. But you may call this function with time=0 and JD0,
+floating-point numbers. The argument jd0 should be an integer and time should
+be a fraction of the day. But you may call this function with time=0 and jd0,
 the desired time, if you don't take care about precision.
 
 # Arguments
-- `JD0::Float64`: JD0+time must be equal to the Julian date for the time coordinate corresponding to the ephemeris (usually TDB or TCB)
-- `time::Float64`: JD0+time must be equal to the Julian date for the time coordinate corresponding to the ephemeris (usually TDB or TCB)
+- `jd0::Float64`: jd0+time must be equal to the Julian Day for the time coordinate corresponding to the ephemeris (usually TDB or TCB)
+- `time::Float64`: jd0+time must be equal to the Julian Day for the time coordinate corresponding to the ephemeris (usually TDB or TCB)
 - `target::Int`: The body or reference point whose coordinates are required. The numbering system depends on the parameter unit.
 - `center::Int`: The origin of the coordinate system. The numbering system depends on the parameter unit.
 - `unit::Int` : The units of the result. This integer is a sum of some unit constants (unit*) and/or the constant useNaifId. If the unit contains useNaifId, the NAIF identification numbering system is used for the target and the center. If the unit does not contain useNaifId, the old number system is used for the target and the center.
 
 """
-function compute(eph::Ephem,JD0::Float64,time::Float64,
+function compute(eph::Ephem,jd0::Float64,time::Float64,
    target::Int64,center::Int64,unit::Int64)
     @_checkPointer eph.data "Ephemeris is not properly initialized!"
     result = Array{Float64,1}(6)
     stat = ccall((:calceph_compute_unit, libcalceph), Cint,
     (Ptr{Void},Cdouble,Cdouble,Cint,Cint,Cint,Ref{Cdouble}),
-    eph.data,JD0,time,target,center,unit,result)
+    eph.data,jd0,time,target,center,unit,result)
     @_checkStatus stat "Unable to compute ephemeris"
     return result
 end
 
 """
-    compute(eph,JD0,time,target,center,unit,order)
+    compute(eph,jd0,time,target,center,unit,order)
 
 Compute position and derivatives up to order of target with respect to center
-at epoch JD0+time.
+at epoch jd0+time.
 To get the best precision for the interpolation, the time is splitted in two
-floating-point numbers. The argument JD0 should be an integer and time should
-be a fraction of the day. But you may call this function with time=0 and JD0,
+floating-point numbers. The argument jd0 should be an integer and time should
+be a fraction of the day. But you may call this function with time=0 and jd0,
 the desired time, if you don't take care about precision.
 
 # Arguments
-- `JD0::Float64`: JD0+time must be equal to the Julian date for the time coordinate corresponding to the ephemeris (usually TDB or TCB)
-- `time::Float64`: JD0+time must be equal to the Julian date for the time coordinate corresponding to the ephemeris (usually TDB or TCB)
+- `jd0::Float64`: jd0+time must be equal to the Julian Day for the time coordinate corresponding to the ephemeris (usually TDB or TCB)
+- `time::Float64`: jd0+time must be equal to the Julian Day for the time coordinate corresponding to the ephemeris (usually TDB or TCB)
 - `target::Int`: The body or reference point whose coordinates are required. The numbering system depends on the parameter unit.
 - `center::Int`: The origin of the coordinate system. The numbering system depends on the parameter unit.
 - `unit::Int` : The units of the result. This integer is a sum of some unit constants (unit*) and/or the constant useNaifId. If the unit contains useNaifId, the NAIF identification numbering system is used for the target and the center. If the unit does not contain useNaifId, the old number system is used for the target and the center.
@@ -104,14 +104,14 @@ the desired time, if you don't take care about precision.
     * 3: the position, velocity and acceleration and jerk are computed.
 
 """
-function compute(eph::Ephem,JD0::Float64,time::Float64,
+function compute(eph::Ephem,jd0::Float64,time::Float64,
    target::Int64,center::Int64,unit::Int64,order::Int64)
     @_checkPointer eph.data "Ephemeris is not properly initialized!"
     @_checkOrder order
     result = Array{Float64,1}(3+3order)
     stat = ccall((:calceph_compute_order, libcalceph), Cint,
     (Ptr{Void},Cdouble,Cdouble,Cint,Cint,Cint,Cint,Ref{Cdouble}),
-    eph.data,JD0,time,target,center,unit,order,result)
+    eph.data,jd0,time,target,center,unit,order,result)
     @_checkStatus stat "Unable to compute ephemeris"
     return result
 end
--- src/orient.jl
+++ src/orient.jl
@@ -1,44 +1,44 @@
 """
-    orient(eph,JD0,time,target,unit)
+    orient(eph,jd0,time,target,unit)
 
 Compute Euler angles and first derivative for the orientation of target at
-epoch JD0+time.
+epoch jd0+time.
 To get the best precision for the interpolation, the time is splitted in two
-floating-point numbers. The argument JD0 should be an integer and time should
-be a fraction of the day. But you may call this function with time=0 and JD0,
+floating-point numbers. The argument jd0 should be an integer and time should
+be a fraction of the day. But you may call this function with time=0 and jd0,
 the desired time, if you don't take care about precision.
 
 # Arguments
-- `JD0::Float64`: JD0+time must be equal to the Julian date for the time coordinate corresponding to the ephemeris (usually TDB or TCB)
-- `time::Float64`: JD0+time must be equal to the Julian date for the time coordinate corresponding to the ephemeris (usually TDB or TCB)
+- `jd0::Float64`: jd0+time must be equal to the Julian Day for the time coordinate corresponding to the ephemeris (usually TDB or TCB)
+- `time::Float64`: jd0+time must be equal to the Julian Day for the time coordinate corresponding to the ephemeris (usually TDB or TCB)
 - `target::Int`: The body or reference point whose orientation is required. The numbering system depends on the parameter unit.
 - `unit::Int` : The units of the result. This integer is a sum of some unit constants (unit*) and/or the constant useNaifId. If the unit contains useNaifId, the NAIF identification numbering system is used for the target and the center. If the unit does not contain useNaifId, the old number system is used for the target and the center (see the list in the documentation of function compute). The angles are expressed in radians if unit contains unitRad.
 
 """
-function orient(eph::Ephem,JD0::Float64,time::Float64,
+function orient(eph::Ephem,jd0::Float64,time::Float64,
    target::Int64,unit::Int64)
     @_checkPointer eph.data "Ephemeris is not properly initialized!"
     result = Array{Float64,1}(6)
     stat = ccall((:calceph_orient_unit, libcalceph), Cint,
     (Ptr{Void},Cdouble,Cdouble,Cint,Cint,Ref{Cdouble}),
-    eph.data,JD0,time,target,unit,result)
+    eph.data,jd0,time,target,unit,result)
     @_checkStatus stat "Unable to compute ephemeris"
     return result
 end
 
 """
-    orient(eph,JD0,time,target,unit,order)
+    orient(eph,jd0,time,target,unit,order)
 
 Compute Euler angles and derivatives up to order for the orientation of target
-at epoch JD0+time.
+at epoch jd0+time.
 To get the best precision for the interpolation, the time is splitted in two
-floating-point numbers. The argument JD0 should be an integer and time should
-be a fraction of the day. But you may call this function with time=0 and JD0,
+floating-point numbers. The argument jd0 should be an integer and time should
+be a fraction of the day. But you may call this function with time=0 and jd0,
 the desired time, if you don't take care about precision.
 
 # Arguments
-- `JD0::Float64`: JD0+time must be equal to the Julian date for the time coordinate corresponding to the ephemeris (usually TDB or TCB)
-- `time::Float64`: JD0+time must be equal to the Julian date for the time coordinate corresponding to the ephemeris (usually TDB or TCB)
+- `jd0::Float64`: jd0+time must be equal to the Julian Day for the time coordinate corresponding to the ephemeris (usually TDB or TCB)
+- `time::Float64`: jd0+time must be equal to the Julian Day for the time coordinate corresponding to the ephemeris (usually TDB or TCB)
 - `target::Int`: The body or reference point whose orientation is required. The numbering system depends on the parameter unit.
 - `unit::Int` : The units of the result. This integer is a sum of some unit constants (unit*) and/or the constant useNaifId. If the unit contains useNaifId, the NAIF identification numbering system is used for the target and the center. If the unit does not contain useNaifId, the old number system is used for the target and the center (see the list in the documentation of function compute).
 - `order::Int` : The order of derivatives
@@ -48,14 +48,14 @@ the desired time, if you don't take care about precision.
     * 3: the angles, the 1st derivatives, 2nd derivatives and 3rd derivatives are computed.
 
 """
-function orient(eph::Ephem,JD0::Float64,time::Float64,
+function orient(eph::Ephem,jd0::Float64,time::Float64,
    target::Int64,unit::Int64,order::Int64)
     @_checkPointer eph.data "Ephemeris is not properly initialized!"
     @_checkOrder order
     result = Array{Float64,1}(3+3order)
     stat = ccall((:calceph_orient_order, libcalceph), Cint,
     (Ptr{Void},Cdouble,Cdouble,Cint,Cint,Cint,Ref{Cdouble}),
-    eph.data,JD0,time,target,unit,order,result)
+    eph.data,jd0,time,target,unit,order,result)
     @_checkStatus stat "Unable to compute ephemeris"
     return result
 end
