20d1922528c981f83434d18ab8e5042be86d5272
Bernard Godard <bgodard@users.noreply.github.com>
2018-02-06 20:22:02 +0100

    upgrade to CALCEPH 3.0

--- deps/build.jl
+++ deps/build.jl
@@ -3,7 +3,7 @@ using BinDeps
 @BinDeps.setup
 
 libcalceph = library_dependency("libcalceph")
-provides(Sources,URI("https://www.imcce.fr/content/medias/recherche/equipes/asd/calceph/calceph-2.3.2.tar.gz"), libcalceph)
+provides(Sources,URI("https://www.imcce.fr/content/medias/recherche/equipes/asd/calceph/calceph-3.0.0.tar.gz"), libcalceph)
 
 provides(BuildProcess,Autotools(configure_options =
     ["--enable-shared", "--disable-fortran", "--disable-python"],
