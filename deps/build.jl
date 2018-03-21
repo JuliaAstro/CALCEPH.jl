3cc7e9e7e070f5fb1f98abb0e8ffbd9c003d8820
Elliot Saba <staticfloat@gmail.com>
2018-03-21 11:34:09 -0700

    More reversion

--- deps/build.jl
+++ deps/build.jl
@@ -42,7 +42,7 @@ libcalceph = library_dependency("libcalceph")
 provides(Sources,URI("https://www.imcce.fr/content/medias/recherche/equipes/asd/calceph/calceph-3.0.0.tar.gz"), libcalceph)
 
 provides(BuildProcess,Autotools(configure_options =
-    ["--enable-shared", "--disable-fortran", "--disable-python"],
-    libtarget=joinpath("src", "libcalceph.la")),libcalceph, os = :Unix)
+        ["--enable-shared", "--disable-fortran", "--disable-python"],
+        libtarget=joinpath("src", "libcalceph.la")),libcalceph, os = :Unix)
 
 @BinDeps.install Dict(:libcalceph => :libcalceph)
