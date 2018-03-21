6b08dc69f1f3df1cd9e19525e370377df5dfb7e0
Elliot Saba <staticfloat@gmail.com>
2018-03-21 11:31:01 -0700

    Revert `using BinDeps` in conditional

--- deps/build.jl
+++ deps/build.jl
@@ -30,18 +30,19 @@ if platform_key() in keys(download_info)
 
     # Write out our deps.jl file that will contain a `libcalceph`
     write_deps_file(joinpath(@__DIR__, "deps.jl"), products)
-else
-    info("Could not find a binary for your platform $(triplet(platform_key())). Will attempt a build.")
-
-    using BinDeps
-    @BinDeps.setup
-
-    libcalceph = library_dependency("libcalceph")
-    provides(Sources,URI("https://www.imcce.fr/content/medias/recherche/equipes/asd/calceph/calceph-3.0.0.tar.gz"), libcalceph)
-
-    provides(BuildProcess,Autotools(configure_options =
-        ["--enable-shared", "--disable-fortran", "--disable-python"],
-        libtarget=joinpath("src", "libcalceph.la")),libcalceph, os = :Unix)
-
-    @BinDeps.install Dict(:libcalceph => :libcalceph)
+    exit(0)
 end
+
+info("Could not find a binary for your platform $(triplet(platform_key())). Will attempt a build.")
+
+using BinDeps
+@BinDeps.setup
+
+libcalceph = library_dependency("libcalceph")
+provides(Sources,URI("https://www.imcce.fr/content/medias/recherche/equipes/asd/calceph/calceph-3.0.0.tar.gz"), libcalceph)
+
+provides(BuildProcess,Autotools(configure_options =
+    ["--enable-shared", "--disable-fortran", "--disable-python"],
+    libtarget=joinpath("src", "libcalceph.la")),libcalceph, os = :Unix)
+
+@BinDeps.install Dict(:libcalceph => :libcalceph)
